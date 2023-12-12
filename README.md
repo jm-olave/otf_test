4640335ad66390acc6b1551dea34340ddb8492fe
jm-olave <jm.olave@uniandes.edu.co>
2023-12-11 21:15:01 -0500

    cambios

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
-# otf_test
+# Rick and Morty API to HubSpot Integration
+
+This repository contains a Node.js application for migrating data from the Rick and Morty API to HubSpot. The integration includes migrating locations as companies and characters as contacts.
+
+## Prerequisites
+
+- Node.js installed
+- HubSpot API key (set as CLIENT_ID_SOURCE in .env file)
+- Rick and Morty API access
+
+## Setup
+
+1. Clone the repository:
+
+   ```bash
+   git clone https://github.com/your-username/your-repo.git
+
+2. Install dependencies:
+- run command:
+    npm install
+
+3. Set up environment variables:
+- Create a .env file in the root directory and add the following:
+CLIENT_ID_SOURCE=your_hubspot_api_key
+BASE_URL=https://api.hubapi.com
+SCOPES=your_scopes( if any)
+CLIENT_SECRET=your_client_secret (if any)
+
+4. Run the application:
+- npm start
+
+## Endpoints
+* GET /get-locations:
+Fetches locations from the Rick and Morty API.
+* GET /fetch-characters:
+Fetches characters with prime IDs from the Rick and Morty API.
+* POST /migrate-locations:
+Migrates locations as companies to HubSpot.
+* POST /migrate-characters:
+Migrates characters to HubSpot.
+* POST /sync-contacts:
+Synchronizes contacts with prime character IDs to HubSpot.
+* POST /sync-companies:
+Synchronizes locations as companies to HubSpot.
+
+## Usage
+* Use the provided endpoints to fetch, migrate, and sync data.
+
+* Check the console logs for migration progress.
--- index.js
+++ index.js
@@ -1,208 +1,392 @@
-const hubspot = require('@hubspot/api-client');
-const express = require('express');
-const axios = require('axios');
-const app = express();
-const PORT = 8080;
-require('dotenv').config();
-const { CLIENT_ID_SOURCE, BASE_URL, SCOPES, CLIENT_SECRET } = process.env;
+/*
+  Author: Your Name
+  Description: This API migrates Rick and Morty characters to HubSpot contacts.
+*/
 
+const hubspot = require("@hubspot/api-client");
+const express = require("express");
+const axios = require("axios");
+const app = express();
+app.use(express.json());
+const PORT = 8080;
+require("dotenv").config();
+const { CLIENT_ID_SOURCE, CLIENT_ID_MIRROR, BASE_URL, SCOPES, CLIENT_SECRET } =
+  process.env;
+const hubspotApiKey = CLIENT_ID_SOURCE;
+const hubspotApiKeyMirror = CLIENT_ID_MIRROR;
+const hubspotClient = new hubspot.Client({ accessToken: hubspotApiKey });
+const hubspotClientMirror = new hubspot.Client({
+  accessToken: hubspotApiKeyMirror,
+});
 /**
  *  code
  */
-app.listen(
-    PORT,
-    () => console.log(`its alive uwu on ${PORT}`)
-);
+app.listen(PORT, () => console.log(`its alive uwu on ${PORT}`));
 
+// fetch logic
 // fetch characters with id = primer number
 const isPrime = (num) => {
-    if (num <= 1) return false;
-    if (num === 2) return true;
-    if (num % 2 === 0) return false;
-  
-    for (let i = 3; i <= Math.sqrt(num); i += 2) {
-      if (num % i === 0) return false;
-    }
-  
-    return true;
-  };
-  // Function to fetch characters with prime IDs
-  const fetchPrimeCharacters = async () => {
-    const primeCharacters = [];
-    // total characters according to the api doc
-    const maxCharacterId =826; 
-    // agrega a rick
-    const character1Response = await axios.get('https://rickandmortyapi.com/api/character/1');
-    primeCharacters.push(character1Response.data);
-    // todos los primos
-    for (let i = 2; i <= maxCharacterId; i++) {
-      if (isPrime(i)) {
-        const response = await axios.get(`https://rickandmortyapi.com/api/character/${i}`);
-        primeCharacters.push(response.data);
-      }
-    }
-  
-    return primeCharacters;
-  };
+  if (num <= 1) return false;
+  if (num === 2) return true;
+  if (num % 2 === 0) return false;
 
-const getLocations = async () =>{
+  for (let i = 3; i <= Math.sqrt(num); i += 2) {
+    if (num % i === 0) return false;
+  }
+
+  return true;
+};
+// Function to fetch characters with prime IDs
+const fetchPrimeCharacters = async () => {
+  const primeCharacters = [];
+  // total characters according to the api doc
+  const maxCharacterId = 826;
+  // agrega a rick
+  const character1Response = await axios.get(
+    "https://rickandmortyapi.com/api/character/1"
+  );
+  primeCharacters.push(character1Response.data);
+  // todos los primos
+  for (let i = 2; i <= maxCharacterId; i++) {
+    if (isPrime(i)) {
+      const response = await axios.get(
+        `https://rickandmortyapi.com/api/character/${i}`
+      );
+      primeCharacters.push(response.data);
+    }
+  }
+
+  return primeCharacters;
+};
+const getLocations = async () => {
   try {
-    const response = await axios.get('https://rickandmortyapi.com/api/location');
+    const response = await axios.get(
+      "https://rickandmortyapi.com/api/location"
+    );
     const locations = response.data.results;
     return locations;
   } catch (error) {
-    console.error('Error fetching data from Rick and Morty API:', error.message);
+    console.error(
+      "Error fetching data from Rick and Morty API:",
+      error.message
+    );
     throw error; // Rethrow the error to be caught by the calling function
   }
 };
 
-// migrations
+// migrations logic
 
-const hubspotApiKey = CLIENT_ID_SOURCE;
-const hubspotClient = new hubspot.Client({"accessToken":hubspotApiKey});
 let companiesMigrated = [];
-// function to migrate locations to hubspot
-const migrateLocationAsCompaniesToHubspot = async(location) =>{
+// function to migrate locations to hubspot on source platform
+const migrateLocationAsCompaniesToHubspot = async (location) => {
   const company = {
     inputs: [
-      {properties : {
-        name: location.name,
-        location_type: location.type,
-        dimension: location.dimension,
-        creation_date: location.created
-      }}
-    ]
+      {
+        properties: {
+          name: location.name,
+          location_type: location.type,
+          dimension: location.dimension,
+          creation_date: location.created,
+        },
+      },
+    ],
   };
-
   try {
-    const apiResponse = await hubspotClient.crm.companies.batchApi.create(company);
+    const apiResponse = await hubspotClient.crm.companies.batchApi.create(
+      company
+    );
     console.log(apiResponse);
     const responseObj = apiResponse;
-    const companyforMigration = responseObj.results.map(result => [result.id, result.properties.name]);
+    const companyforMigration = responseObj.results.map((result) => [
+      result.id,
+      result.properties.name,
+    ]);
     companiesMigrated.push(companyforMigration);
     // console.log(JSON.stringify(apiResponse, null, 2));
     return apiResponse;
   } catch (error) {
-    console.error('Error migrating Company to HubSpot:', error.message);
+    console.error("Error migrating Company to HubSpot:", error.message);
+    throw error;
+  }
+};
+
+// function to migrate locations to hubspot on mirror platform
+const integrateLocationToHubspotMirrorCreate = async (location) => {
+  const company = {
+    inputs: [
+      {
+        properties: {
+          name: location.name,
+          location_type: location.type,
+          dimension: location.dimension,
+          creation_date: location.created,
+        },
+      },
+    ],
+  };
+  try {
+    const apiResponse = await hubspotClientMirror.crm.companies.batchApi.create(
+      company
+    );
+    console.log(apiResponse);
+    const responseObj = apiResponse;
+    const companyforMigration = responseObj.results.map((result) => [
+      result.id,
+      result.properties.name,
+    ]);
+    companiesMigrated.push(companyforMigration);
+    // console.log(JSON.stringify(apiResponse, null, 2));
+    return apiResponse;
+  } catch (error) {
+    console.error("Error migrating Company to HubSpot:", error.message);
+    throw error;
+  }
+};
+// update company
+const integrateLocationToHubspotMirrorUpdate = async (location, id) => {
+  console.log(location, id);
+  const company = {
+    inputs: [
+      {
+        id: id,
+        properties: {
+          ...(location.name && { name: location.name }),
+          ...(location.type && { location_type: location.type }),
+          ...(location.dimension && { dimension: location.dimension }),
+          ...(location.created && { creation_date: location.created }),
+        },
+      },
+    ],
+  };
+  try {
+    const apiResponse = await hubspotClientMirror.crm.companies.batchApi.update(
+      company
+    );
+    console.log(JSON.stringify(apiResponse, null, 2));
+    console.log(company);
+    // console.log(JSON.stringify(apiResponse, null, 2));
+    return apiResponse;
+  } catch (error) {
+    console.error("Error migrating Company to HubSpot:", error.message);
     throw error;
   }
 };
 // Function to migrate character to HubSpot
+
 const migrateCharactersToHubSpot = async (character) => {
-  
   let namePartition = character.name.split(" ");
   let firstname = " ";
   let lastname = " ";
-  if(namePartition.length < 3){
+  if (namePartition.length < 3) {
     firstname = namePartition[0];
     lastname = namePartition[1];
-  }
-  else{
+  } else {
     firstname = character.name;
   }
   // association to location
-const matchingLocation = companiesMigrated.find(tuple => tuple[0] && tuple[0][1] === character.location?.name);
-const idToAssociate = matchingLocation ? matchingLocation[0][0] : null;
+  const matchingLocation = companiesMigrated.find(
+    (tuple) => tuple[0] && tuple[0][1] === character.location?.name
+  );
+  const idToAssociate = matchingLocation ? matchingLocation[0][0] : null;
 
-let contact = {
-  inputs: [
-    {
-      properties: {
-        firstname: firstname,
-        lastname: lastname,
-        status_character: character.status,
-        character_species: character.species,
-        character_gender: character.gender
-      }
-    }
-  ]
-};
-
-if (idToAssociate !== null && idToAssociate !== undefined) {
-  // Include association only if idToAssociate is valid
-  contact.inputs[0].associations = [
-    {
-      to: {
-        id: idToAssociate
+  let contact = {
+    inputs: [
+      {
+        properties: {
+          firstname: firstname,
+          lastname: lastname,
+          status_character: character.status,
+          character_species: character.species,
+          character_gender: character.gender,
+        },
       },
-      types: [
-        {
-          associationCategory: "HUBSPOT_DEFINED",
-          associationTypeId: 279
-        } ]
-    }
-  ];
-  console.log("contact being migrated:", contact)
-}
+    ],
+  };
+
+  if (idToAssociate !== null && idToAssociate !== undefined) {
+    // Include association only if idToAssociate is valid
+    contact.inputs[0].associations = [
+      {
+        to: {
+          id: idToAssociate,
+        },
+        types: [
+          {
+            associationCategory: "HUBSPOT_DEFINED",
+            associationTypeId: 279,
+          },
+        ],
+      },
+    ];
+    console.log("contact being migrated:", contact);
+  }
 
   try {
-    const apiResponse = await hubspotClient.crm.contacts.batchApi.create(contact);
+    const apiResponse = await hubspotClient.crm.contacts.batchApi.create(
+      contact
+    );
     // console.log(JSON.stringify(apiResponse, null, 2));
     return apiResponse;
   } catch (error) {
-    console.error('Error migrating Contact to HubSpot:', error.message);
+    console.error("Error migrating Contact to HubSpot:", error.message);
+    throw error;
+  }
+};
+// integration character logic
+const integrateCharactersToHubSpotMirrorCreate = async (character) => {
+  const contact = {
+    inputs: [
+      {
+        properties: {
+          firstname: character.firstname,
+          lastname: character.lastname,
+          status_character: character.status,
+          character_species: character.species,
+          character_gender: character.gender,
+        },
+        ...(character.associations && { associations: character.associations }),
+      },
+    ],
+  };
+  try {
+    const apiResponse = await hubspotClientMirror.crm.contacts.batchApi.create(
+      contact
+    );
+    console.log(JSON.stringify(apiResponse, null, 2));
+    return apiResponse;
+  } catch (error) {
+    console.error("Error migrating Contact to HubSpot:", error.message);
+    throw error;
+  }
+};
+const integrateCharactersToHubSpotMirrorUpdate = async (character, id) => {
+  const contact = {
+    inputs: [
+      { id: id,
+        properties: {
+          firstname: character.firstname,
+          lastname: character.lastname,
+          status_character: character.status,
+          character_species: character.species,
+          character_gender: character.gender,
+        },
+        ...(character.associations && { associations: character.associations }),
+      },
+    ],
+  };
+  try {
+    const apiResponse = await hubspotClientMirror.crm.contacts.batchApi.update(
+      contact
+    );
+    // console.log(JSON.stringify(apiResponse, null, 2));
+    return apiResponse;
+  } catch (error) {
+    console.error("Error migrating Contact to HubSpot:", error.message);
     throw error;
   }
 };
 
-// endpoint logic 
-app.get('/get-locations', async (req, res) => {
+// endpoints
+app.get("/get-locations", async (req, res) => {
   try {
     const locations = await getLocations();
     res.json({ locations: locations });
   } catch (error) {
-    console.error('Error fetching data from Rick and Morty API:', error.message);
-    res.status(500).json({ error: 'Internal Server Error' });
+    console.error(
+      "Error fetching data from Rick and Morty API:",
+      error.message
+    );
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
-  app.get('/fetch-characters', async (req, res) => {
-    try {
-      const characters = await fetchPrimeCharacters();
-      res.json({ characters: characters });
-    } catch (error) {
-      console.error('Error fetching data from Rick and Morty API:', error.message);
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
-  });
+app.get("/fetch-characters", async (req, res) => {
+  try {
+    const characters = await fetchPrimeCharacters();
+    res.json({ characters: characters });
+  } catch (error) {
+    console.error(
+      "Error fetching data from Rick and Morty API:",
+      error.message
+    );
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
 // migrate locations as companies
-app.post('/migrate-locations', async (req, res) => {
+app.post("/migrate-locations", async (req, res) => {
   try {
     const locations = await getLocations();
     for (const location of locations) {
       migrateLocationAsCompaniesToHubspot(location);
     }
-  return res.status(200).json({message: "completed migration for characters"})
+    return res
+      .status(200)
+      .json({ message: "completed migration for characters" });
   } catch (error) {
-    console.error('Error migrating locations to HubSpot:', error.message);
-    res.status(500).json({ error: 'Internal Server Error' });
+    console.error("Error migrating locations to HubSpot:", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 // Route to migrate characters to HubSpot
-app.post('/migrate-characters', async (req, res) => {
+app.post("/migrate-characters", async (req, res) => {
   try {
     const characters = await fetchPrimeCharacters();
     // Migrate each character to HubSpot
     for (const character of characters) {
       migrateCharactersToHubSpot(character);
     }
-    return res.status(200).json({message: "completed migration for characters"})
+    return res
+      .status(200)
+      .json({ message: "completed migration for characters" });
   } catch (error) {
-    console.error('Error migrating characters to HubSpot:', error.message);
-    res.status(500).json({ error: 'Internal Server Error' });
+    console.error("Error migrating characters to HubSpot:", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
-app.post('/create-associations', async (req, res) => {
+app.post("/sync-locations", async (req, res) => {
   try {
-    const characters = await fetchPrimeCharacters();
-    // Migrate each character to HubSpot
-    for (const character of characters) {
-      migrateCharactersToHubSpot(character);
+    const location = {
+      name: req.body.name,
+      type: req.body.type,
+      dimension: req.body.dimension,
+      created: req.body.created,
+    };
+    if (!req.body.id) {
+      integrateLocationToHubspotMirrorCreate(location);
+    } else {
+      console.log("pasa el if de actualizacion");
+      integrateLocationToHubspotMirrorUpdate(location, req.body.id);
     }
-    return res.status(200).json({message: "completed migration for characters"})
+
+    return res
+      .status(200)
+      .json({ message: "completed integratioon of locations" });
   } catch (error) {
-    console.error('Error migrating characters to HubSpot:', error.message);
-    res.status(500).json({ error: 'Internal Server Error' });
+    console.error("Error integrating too hubspot:", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
+app.post("/sync-characters", async (req, res) => {
+  try {
+    const contact = {
+          firstname: req.body.firstname,
+          lastname: req.body.lastname,
+          status_character: req.body.status,
+          character_species: req.body.species,
+          character_gender: req.body.gender,
+          associations: req.body.associations,
+    };
+    if (!req.body.id) {
+      integrateCharactersToHubSpotMirrorCreate(contact);
+    } else {
+      console.log("pasa el if de actualizacion");
+      integrateCharactersToHubSpotMirrorUpdate(contact, req.body.id);
+    }
 
-
+    return res
+      .status(200)
+      .json({ message: "completed integratioon of locations" });
+  } catch (error) {
+    console.error("Error integrating too hubspot:", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
